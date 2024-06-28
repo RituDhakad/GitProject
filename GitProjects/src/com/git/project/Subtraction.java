d4e43f1e445f18bc7f1ee3f86eee6dec39c168a6
ritu881github <ritudhakad881@mail.com>
2024-06-28 13:05:25 +0530

    Subtraction program

---
+++ GitProjects/src/com/git/project/Subtraction.java
@@ -0,0 +1,15 @@
+package com.git.project;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		int subtraction = b - a;
+
+		System.out.println("Subtraction is>> " + subtraction);
+
+	}
+}
